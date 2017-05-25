a241190301809663ec29b0308cb1ba8b1473e48e
文国邦 <handsomewgb@163.com>
2017-05-25 22:51:09 +0800

    迭代器/将二叉树转换为双向列表

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,37 +2,158 @@
 #include<iostream>
 using namespace std;
 
+#include<vector>
+
 
 template <class K,class V>
 struct BSTNode
 {
-	BSTNode(const K& key,const V& value)
+	BSTNode(const K& key=K(),const V& value= V())
 	:_pLeft(NULL)
 	, _pRight(NULL)
+	, _parent(NULL)
 	, _key(key)
 	, _value(value)
 	{}
 	BSTNode<K, V>* _pLeft;
 	BSTNode<K, V>* _pRight;
+	BSTNode<K, V>* _parent;
 	K _key;
 	V _value;
 };
 
+template <class K, class V, class Ref, class Ptr>
+class BinarySearchTreeIterator
+{
+	typedef BSTNode<K, V> Node;
+	typedef BinarySearchTreeIterator<K, V, Ref, Ptr> self;
+	typedef Node* ptr;
+public:
+	BinarySearchTreeIterator()
+		:_pNode(NULL)
+	{}
+	BinarySearchTreeIterator(const self& s)
+		:_pNode(s._pNode)
+	{}
+	BinarySearchTreeIterator(Node* pNode)
+		:_pNode(pNode)
+	{}
 
+	self& operator++()
+	{
+		Increment(_pNode);
+		return *this;
+	}
+	self operator++(int)
+	{
+		self temp(*this);
+		Increment(_pNode);
+		return temp;
+	}
+	self& operator--()
+	{
+		Decrement(_pNode);
+		return *this;
+	}
+	self operator--(int)
+	{
+		self temp(*this);
+		Decrement(_pNode);
+		return *this;
+	}
+	Ref& operator*()
+	{
+		return _pNode->_key;
+	}
+	Ptr operator->()
+	{
+		return &(operator*());
+	}
+	bool operator==(const self& s)
+	{
+		return _pNode == s._pNode;
+	}
+	bool operator!=(const self& s)
+	{
+		return _pNode != s._pNode;
+	}
+
+protected:
+	Node*  Increment(Node* pNode)
+	{
+		//当前结点如果他的右子树存在，找右子树中最左边的结点
+		if (pNode->_pRight)
+		{
+			pNode = pNode->_pRight;
+			while (pNode->_pLeft)//注意条件是pNode的左孩子不为空，不是pNode
+				pNode = pNode->_pLeft;
+		}
+		else
+		{
+			Node* parent = pNode->_parent;
+			while (parent->_pRight == pNode)
+			{
+				pNode = parent;
+				parent = parent->_parent;
+			}
+			if (pNode->_pRight != parent)//????????
+				pNode = parent;
+		}
+		_pNode = pNode;
+		return pNode;
+	}
+	Node* Decrement(Node* pNode)
+	{
+		if (pNode->_pLeft)
+		{//左孩子存在，找左孩子的右孩子中最大的
+			pNode = pNode->_pLeft;
+			while (pNode->_pRight)
+				pNode = pNode->_pRight;
+		}
+		else
+		{
+			//左单支的情况
+			Node* parent = pNode->_parent;
+			while (parent->_pLeft == pNode)
+			{
+				pNode = parent;
+				parent = parent->_parent;
+			}
+			pNode = parent;
+		}
+		_pNode = pNode;
+		return pNode;
+	}
+protected:
+	Node* _pNode;
+};
 template <class K,class V>
 class BinarySearchTree
 {
 	typedef BSTNode<K, V> Node;
 	typedef BinarySearchTree<K, V> Self;
-
+public:
+	typedef BinarySearchTreeIterator<K,V,K&,K*> Iterator;
 public:
 	BinarySearchTree()
-		:_pRoot(NULL)
-	{}
+	{
+		_pRoot = new Node();
+		_pRoot->_pLeft = _pRoot;
+		_pRoot->_pRight = _pRoot;
+		_pRoot->_parent = NULL;
+	}
+	Iterator Begin()
+	{
+		return Iterator(_pRoot->_pLeft);
+	}
+	Iterator End()
+	{
+		return Iterator(_pRoot);
+	}
 
 	BinarySearchTree(const Self& BST)
 	{
-		_pRoot = _CopyBinarySearchTree(BST._pRoot);
+		_pRoot = _CopyBinarySearchTree(BST._pRoot);//先序拷贝
 	}
 	Self& operator=(const Self& BST)
 	{
@@ -51,41 +172,40 @@ public:
 			return false;
 		//树不空
 		Node* pCur = _pRoot;
-		while (pCur)
+		while (pCur)//
 		{
 			if (pCur->_key > key)
-			{
 				pCur = pCur->_pLeft;
-			}
 			else if (pCur->_key < key)
-			{
 				pCur = pCur->_pRight;
-			}
 			else
-			{
 				return true;
-			}
 		}
 		return false;
 	}
 	bool Find(const K& key)
 	{
-		return _Find(_pRoot,key);
+		return _Find(GetRoot(),key);
 	}
 	///////////////////////////////////
 	//插入
 	bool Insert_Nor(const K& key, const V& value)
 	{
 		//树空，直接插入
-		if (NULL == _pRoot)
+		//++++
+		Node* pRoot = GetRoot();
+		if (NULL == pRoot)
 		{
-			_pRoot = new Node(key,value);
+			pRoot = new Node(key, value);
+			//++++
+			_pRoot->_parent = pRoot;
+			pRoot->_parent = _pRoot;
 			return true;
 		}
-		//查找要插入的元素
-		Node* pCur = _pRoot;
+		//查找要插入的位置
+		Node* pCur = pRoot;
 		Node* parent = NULL;
-		while (pCur)
+		while (pCur)//查找要插入的位置
 		{
 			if (pCur->_key > key)
 			{
@@ -99,34 +219,34 @@ public:
 			}
 			else
 			{
-				return false;
+				return false;//该元素存在，
 			}
 		}
 		//要插入的在树中不存在，在寻找停止的位置插入
-		if (NULL == pCur)
-		{
-			if (parent->_key > key)
-			{
-				parent->_pLeft = new Node(key,value);
-			}
-			else
-			{
-				parent->_pRight = new Node(key,value);
-			}
-		}
+		pCur = new Node(key,value);
+		if (parent->_key > key)
+			parent->_pLeft = pCur;
+		else
+			parent->_pRight = pCur;
+		pCur->_parent = parent;
+
+		//++++
+		_pRoot->_pLeft = GetMinNode();
+		_pRoot->_pRight = GetMaxNode();
 		return true;
 	}
 	bool Insert(const K& key, const V& value)
 	{
-		return _Insert(_pRoot,key,value);
+		return _Insert(GetRoot(),key,value);
 	}
 	/////////////////////////////////////
 	//删除
 	bool Remove_Nor(const K& key)
 	{
-		if (NULL == _pRoot)
+		Node* pRoot = GetRoot();
+		if (NULL == pRoot)
 			return false;
-		Node* pCur = _pRoot;
+		Node* pCur = pRoot;
 		Node* parent = NULL;
 		while (pCur)
 		{
@@ -140,12 +260,13 @@ public:
 				parent = pCur;
 				pCur = pCur->_pRight;
 			}
-			else /*if (pCur->_key == key)*/
+			else 
 			{
-				parent = pCur;
+				parent = pCur;//要删除的元素存在
 				break;
 			}
 		}
+		//pCur为空说明该元素不存在
 		if (NULL == pCur)
 		{
 			return false;
@@ -155,12 +276,12 @@ public:
 		{
 			if (pCur == _pRoot)
 			{
-				_pRoot = pCur->_pRight;
+				pRoot = pCur->_pRight;
 				delete pCur;
 			}
 			else if (pCur == parent->_pRight)
 			{
-				parent->_pRight = pCur;
+				parent->_pRight = pCur->_pRight;
 				delete pCur;
 			}
 			else if (pCur == parent->_pLeft)
@@ -174,7 +295,7 @@ public:
 		{
 			if (pCur == _pRoot)
 			{
-				_pRoot = pCur->_pLeft;
+				pRoot = pCur->_pLeft;
 				delete pCur;
 			}
 			else if (pCur == parent->_pLeft)
@@ -216,13 +337,13 @@ public:
 	}
 	bool Remove(const K& key)
 	{
-		return _Remove(_pRoot,key);
+		return _Remove(GetRoot(),key);
 	}
 
-	void InOrder()
+	void InOrder()//中序
 	{
 		cout << "InOrder" << endl;
-		_InOrder(_pRoot);
+		_InOrder(GetRoot());
 		cout << endl;
 	}
 
@@ -234,7 +355,51 @@ public:
 			_pRoot = NULL;
 		}
 	}
+	Node* ToList()
+	{
+		Node* pRoot = GetRoot();
+		if (NULL == pRoot)
+			return NULL;
+		//找到最左边的结点
+		while (pRoot->_pLeft)
+			pRoot = pRoot->_pLeft;
+
+		Node* prev = NULL;
+		_ToList(GetRoot(),prev);
+		return pRoot;
+	}
 private:
+	void _ToList(Node* pRoot,Node*& prev)
+	{
+		if (NULL == pRoot)
+			return ;
+		_ToList(pRoot->_pLeft,prev);
+
+		pRoot->_pLeft = prev;
+		if (prev)
+			prev->_pRight = pRoot;
+		prev = pRoot;
+
+		_ToList(pRoot->_pRight,prev);
+	}
+	Node*& GetRoot()
+	{
+		return _pRoot->_parent;
+	}
+	Node* GetMinNode()
+	{
+		Node* pCur = GetRoot();
+		while (pCur->_pLeft)
+			pCur = pCur->_pLeft;
+		return pCur;
+	}
+	Node* GetMaxNode()
+	{
+		Node* pCur = GetRoot();
+		while (pCur->_pRight)
+			pCur = pCur->_pRight;
+		return pCur;
+	}
 	Node* _CopyBinarySearchTree(Node* Root)
 	{
 		Node* NewNode = NULL;
@@ -268,15 +433,15 @@ private:
 			Root = new Node(key,value);
 			return true;
 		}
-		if (Root->_key == key && Root->_value == value)
+		else
 		{
-			return false;
+			if (Root->_key > key)
+				_Insert(Root->_pLeft, key, value);
+			else if (Root->_key < key)
+				_Insert(Root->_pRight, key, value);
+			else
+				return false;
 		}
-		if (Root->_key > key)
-			_Insert(Root->_pLeft,key,value);
-		if (Root->_key < key)
-			_Insert(Root->_pRight,key,value);
-		return false;
 	}
 	bool _Remove(Node*& Root, const K& key)
 	{
@@ -366,9 +531,29 @@ void Test2()
 	//else
 	//	cout << "不存在" << endl;
 }
+void Test3()
+{
+	int a[] = { 5, 3/*,4*/, 1, 7, 8, 2, 6,9 };
+	BinarySearchTree<int, int> b;
+	for (size_t idx = 0; idx < sizeof(a) / sizeof(a[0]); ++idx)
+	{
+		b.Insert_Nor(a[idx], a[idx]);
+	}
+	BSTNode<int,int>* pHead = b.ToList();
+	BinarySearchTree<int, int>::Iterator it = b.Begin();
+	BinarySearchTree<int, int>::Iterator it1 = b.End();
+
+	while (it != b.End())
+	{
+		cout << *it << " ";
+		++it;
+	}
+
+}
 int main()
 {
 	//Test1();
-	Test2();
+	//Test2();
+	Test3();
 	return 0;
 }
